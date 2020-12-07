cccbd3d53d870f0cdecd1abd989f3cc93fe9fe83
Angel <atstojanov@gmail.com>
2020-12-07 16:22:47 +0200

    Clean up

--- src/populator.ts
+++ src/populator.ts
@@ -81,69 +81,8 @@ export class Populator {
     await ecoversePopulator.populate();
     await groupPopulator.populate();
     await userPopulator.populate();
-    await organisationPopulator.populate();
-    await challengePopulator.populate();
     await challengePopulator.populate();
     await opportunityPopulator.populate();
+    await organisationPopulator.populate();
   }
-
-  // async loadChallenges(sheetName: string) {
-  //   return this.challengesSheetPopulator.loadChallengesFromSheet(
-  //     sheetName,
-  //     this.gsheetConnector
-  //   );
-  // }
-
-  // async loadUsers(sheetName: string) {
-  //   return this.usersSheetPopulator.loadUsersFromSheet(
-  //     sheetName,
-  //     this.gsheetConnector
-  //   );
-  // }
-
-  // async loadOrganisations(sheetName: string) {
-  //   return this.orgSheetPopulator.loadOrganisationsFromSheet(
-  //     sheetName,
-  //     this.gsheetConnector
-  //   );
-  // }
-
-  // Load users from a particular googlesheet
-  // async loadTeams(sheetName: string) {
-  //   const sheetRange = `${sheetName}!A1:Z1200`;
-  //   const teamsGSheet = await this.gsheetConnector.getObjectArray(sheetRange);
-  //   this.logger.info(
-  //     '==================================================================='
-  //   );
-  //   this.logger.info(
-  //     `====== Obtained gsheet ${sheetRange}  with ${teamsGSheet.length} rows`
-  //   );
-
-  //   // Iterate over the rows
-  //   for (const teamRow of teamsGSheet) {
-  //     const teamName = teamRow['NAME'];
-  //     if (!teamRow) {
-  //       // End of valid teams
-  //       break;
-  //     }
-  //     // const challengeName = teamRow['CHALLENGE'];
-  //     // todo: tag the team with the challenge name
-
-  //     // start processing
-  //     this.logger.info(`Processing team: ${teamName}....`);
-  //     // const teamProfileID = '===> teamCreation - FULL';
-
-  //     try {
-  //       const group = await this.client.createEcoverseGroup(teamName);
-  //       const tagsetId =
-  //         group?.profile?.tagsets && group?.profile?.tagsets[0].id;
-  //       if (tagsetId) {
-  //         await this.client.addTagToTagset(tagsetId, 'Team');
-  //       }
-  //       // Add the "Team" tag to the group
-  //     } catch (e) {
-  //       throw e;
-  //     }
-  //   }
-  // }
 }
