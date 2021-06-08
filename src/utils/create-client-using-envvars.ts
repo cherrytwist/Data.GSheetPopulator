b1380376a56af055e09120ac6ffa1c0f8c28782a
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-06-08 19:26:06 +0300

    Updated Kratos config

--- src/utils/create-client-using-envvars.ts
+++ src/utils/create-client-using-envvars.ts
@@ -5,12 +5,17 @@ export const createClientUsingEnvVars = async () => {
   dotenv.config();
 
   const server = process.env.CT_SERVER || 'http://localhost:4455/graphql';
+  const apiEndpointConfig = () =>
+    process.env.AUTH_ORY_KRATOS_PUBLIC_BASE_URL ?? 'http://localhost:4433/';
   const ctClient = new CherrytwistClient();
   await ctClient.configureGraphqlClient({
     graphqlEndpoint: server,
-    credentials: {
-      email: process.env.AUTH_ADMIN_EMAIL ?? 'admin@cherrytwist.org',
-      password: process.env.AUTH_ADMIN_PASSWORD ?? '!Rn5Ez5FuuyUNc!',
+    authInfo: {
+      credentials: {
+        email: process.env.AUTH_ADMIN_EMAIL ?? 'admin@cherrytwist.org',
+        password: process.env.AUTH_ADMIN_PASSWORD ?? '!Rn5Ez5FuuyUNc!',
+      },
+      apiEndpointFactory: apiEndpointConfig,
     },
   });
   return ctClient;
