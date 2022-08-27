86def2d14e820be8e024840c0047ec1607975b3f
Neil Smyth <neil@thesmyths.eu>
2022-08-27 23:26:51 +0200

    updated to use new client-lib package + how config is loaded

--- src/utils/create-client-using-envvars.ts
+++ src/utils/create-client-using-envvars.ts
@@ -1,22 +1,10 @@
-import * as dotenv from 'dotenv';
 import { AlkemioClient } from '@alkemio/client-lib';
+import { createConfigUsingEnvVars } from './create-config-using-envvars';
 
 export const createClientUsingEnvVars = async () => {
-  dotenv.config();
+  const config = createConfigUsingEnvVars();
 
-  const server =
-    process.env.API_ENDPOINT_PRIVATE_GRAPHQL ||
-    'http://localhost:3000/api/private/non-interactive/graphql';
-  const alkemioClient = new AlkemioClient({
-    apiEndpointPrivateGraphql: server,
-  });
-
-  alkemioClient.config.authInfo = {
-    credentials: {
-      email: process.env.AUTH_ADMIN_EMAIL ?? 'admin@alkem.io',
-      password: process.env.AUTH_ADMIN_PASSWORD ?? 'Fin1.tan',
-    },
-  };
+  const alkemioClient = new AlkemioClient(config);
 
   await alkemioClient.enableAuthentication();
   return alkemioClient;
